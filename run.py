58d1d68f30d6239486eb4e3046599ca89dac4339
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-04-09 09:58:47 +0200

    Working now

--- run.py
+++ run.py
@@ -584,9 +584,9 @@ def page_not_found(e):
     # Tutaj możesz przekierować do dowolnej trasy, którą chcesz wyświetlić jako stronę błędu 404.
     return redirect(url_for(f'index'))
 
-@app.route('/search-post-blog') #, methods=['GET', 'POST']
+@app.route('/search-post-blog', methods=['GET', 'POST']) #, methods=['GET', 'POST']
 def searchBlog():
-    if request.method == "POST":
+    if request.method == "POST" or request.method == "GET":
         query = request.form["query"]
 
         sqlQuery = """
